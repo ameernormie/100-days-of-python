d6b5985abfab2021ec140c6925324936f08cb895
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-09-10 19:53:08 +0500

    chore: define a module words.py that prints words from a url
    add docstring for code readability

---
+++ 2. Python Fundamentals/Modularity/words.py
@@ -0,0 +1,54 @@
+"""Retrieve and print words from a URl
+
+Usage:
+    python3 words.py <URL>
+"""
+
+import sys
+from urllib.request import urlopen
+
+
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a utf-8 document
+
+    Returns:
+        A list of strings containing the words from document.
+    """
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+
+
+def print_items(items):
+    """ Print items one per line
+
+    Args:
+        An iterable series of printable items
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print each word from a text document from a url
+
+    Args:
+        url: The URL of a utf-8 document
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    # Accept the url via a command line argument
+    main(sys.argv[1])
+
+
+# 'http://sixty-north.com/c/t.txt'
