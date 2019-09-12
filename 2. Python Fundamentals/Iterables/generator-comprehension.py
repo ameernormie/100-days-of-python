01a67417ab16bbd12bea2468987e46d7b483bc6b
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-09-12 21:12:27 +0500

    chore: cover generator comprehensions

---
+++ 2. Python Fundamentals/Iterables/generator-comprehension.py
@@ -0,0 +1,21 @@
+"""Generator Comprehensions
+
+Similar syntax to list comprehensions
+Create a generator object
+Concise
+Lazy Evaluation
+
+"""
+
+million_squares = (x*x for x in range(1, 1000001))
+
+""" This returned us a generator function that can be lazily consumed"""
+million_squares         # <generator object <genexpr> at 0x1005d9550>
+
+
+"""To evaluate the generator"""
+# list(million_squares)
+
+
+"""Calculate the sum of first 10 million squares using the built-in sum"""
+sum(x*x for x in range(1, 10000001))
