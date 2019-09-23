158bb60ad904806e989a0e52b9b76ca1b864ae7d
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-09-23 22:35:17 +0500

    chore: add docs

--- Netfilx/matching-pairs.py
+++ Netfilx/matching-pairs.py
@@ -1,4 +1,4 @@
 """
 Given a list of whole positive numbers, find two numbers in that list 
-that sum up to a given target number. Assume there are no duplicates in the list
+that sum up to a given target number. Assume there are no duplicates in the list.
 """
