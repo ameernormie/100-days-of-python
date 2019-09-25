37426910fcfc22c22867c769139a2bee12187baf
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-09-25 09:46:13 +0500

    chore: file management basics

---
+++ 2. Python Fundamentals/File-Management/file.py
@@ -0,0 +1,16 @@
+f = open('wasteland.txt', mode='wt', encoding='utf-8')
+
+f.write('What is going on?')
+f.write('Nothing just \n')
+f.write('chilling and shit')
+
+f.close()
+
+"""Reading a file"""
+g = open('wasteland.txt', mode='rt', encoding='utf-8')
+
+g.read(30)      # Read first 30 characters
+g.read()        # Read rest of the characters
+g.read()        # Will result in empty string because whole file has been read
+
+g.seek(0)       # Seek the file from 0 so it can be read again
