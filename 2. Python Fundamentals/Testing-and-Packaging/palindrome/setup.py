a36c9410f9886da05216932276d2d8d65815ccac
Ameer Hamza <ameer.hamza@emumba.com>
2019-10-01 00:23:34 +0500

    chore: package a module for distribution

---
+++ 2. Python Fundamentals/Testing-and-Packaging/palindrome/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='palindrome',
+    version='1.0',
+    py_modules=['palindrome'],
+
+    # metadata
+    author="Ameer Hamza",
+    author_email="ameernormie@gmail.com",
+    description="A module for verifying palindrome numbers",
+    license="Public domain",
+    keywords="example"
+)
