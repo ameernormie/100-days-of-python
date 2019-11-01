833986d27f02a3f2589d0b1752c1065d3e25ecb8
Ameer Hamza <ameernormie@gmail.com>
2019-11-01 20:16:38 +0500

    chore: use functools to wrap decorator

--- 3. Python - Beyond the basics/3.ClosuresAndDecorators/decorator_metadata_loss.py
+++ 3. Python - Beyond the basics/3.ClosuresAndDecorators/decorator_metadata_loss.py
@@ -4,13 +4,13 @@ and doc of the method that we get by __name__ and __doc__
 method. This could cause trouble if we want to check these things
 """
 
+import functools
+
 
 def noop(f):
+    @functools.wraps(f)
     def noop_wrapper():
         return f()
-
-    noop_wrapper.__name__ = f.__name__
-    noop_wrapper.__doc__ = f.__doc__
     return noop_wrapper
 
 
